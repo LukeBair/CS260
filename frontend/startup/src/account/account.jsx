995ec96a6b11d4def685b46d3d0236183c0cb7c3
LukeBair <74842241+LukeBair@users.noreply.github.com>
2026-02-12 20:23:22 -0700

    stubs

---
+++ frontend/startup/src/account/account.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Account() {
+  return (
+    <main className="container-fluid text-center">
+      <div>account displayed here</div>
+    </main>
+  );
+}
---
+++ frontend/startup/src/everything/everything.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Everything() {
+  return (
+    <main className="container-fluid text-center">
+      <div>everything displayed here</div>
+    </main>
+  );
+}
