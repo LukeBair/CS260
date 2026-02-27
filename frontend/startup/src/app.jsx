debdf8e4bda192fe1a9e1141d6b314c804326309
LukeBair <74842241+LukeBair@users.noreply.github.com>
2026-02-26 21:31:13 -0700

    im stupid, its not duplicate cause its the actuall rendering structure

--- frontend/startup/src/app.jsx
+++ frontend/startup/src/app.jsx
@@ -8,7 +8,38 @@ import {loadUserStoryData} from "./backend/bankendDummy";
 import './app.css';
 
 function SidebarLayout() {
-  return <Outlet />;
+  return (
+    <>
+      <nav id="sidebar">
+        <h2>Mythril</h2>
+        <NavLink to="/everything/story" className="nav-button">
+          <img src="/open-book.png" alt="story icon" className="sidebar-image" />
+          <span>Story</span>
+        </NavLink>
+        <NavLink to="/everything/characters" className="nav-button">
+          <img src="/user.png" alt="characters icon" className="sidebar-image" />
+          <span>Characters</span>
+        </NavLink>
+        <NavLink to="/everything/locations" className="nav-button">
+          <img src="/pin.png" alt="locations icon" className="sidebar-image" />
+          <span>Locations</span>
+        </NavLink>
+        <NavLink to="/everything/props" className="nav-button">
+          <img src="/chest.png" alt="props icon" className="sidebar-image" />
+          <span>Props</span>
+        </NavLink>
+        <NavLink to="/everything/history" className="nav-button">
+          <img src="/hour-glass.png" alt="history icon" className="sidebar-image" />
+          <span>History</span>
+        </NavLink>
+        <NavLink to="/account" className="nav-button account-button">
+          <img src="/cog-wheel-silhouette.png" alt="account icon" className="sidebar-image" />
+          <span>Account</span>
+        </NavLink>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
 
 function NotFound() {
